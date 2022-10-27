8c600539266f6b06541089dae87013e1fa081556
bohdan_hordiychuk <bohdan1217>
2022-10-27 19:26:37 +0300

    small changes

--- app/Exceptions/Iwms/IwmsApiError.php
+++ app/Exceptions/Iwms/IwmsApiError.php
@@ -2,19 +2,27 @@
 
 namespace App\Exceptions\Iwms;
 
+use Exception;
 use Illuminate\Support\Facades\Log;
+use Throwable;
 
-class IwmsApiError extends \Exception
+class IwmsApiError extends Exception
 {
     public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
 
-    public function render()
+    public function render($request)
     {
         Log::error("Code: " . $this->code . " File:" . $this->file .  " Message: " . $this->getMessage());
         Log::error($this);
+
+        if ($request->ajax()) {
+            return response()->json([
+                'error' => $this->getMessage()
+            ], $this->code);
+        }
 
         return redirect()->back()->with('toast_error', $this->getMessage());
     }

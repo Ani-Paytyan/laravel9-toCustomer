9f1a043aaa11b457363b1f6387ad262e4fac3f71
bohdan_hordiychuk <bohdan1217>
2022-11-14 11:07:37 +0200

    WIP

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('guest')->group(function () {
+    Route::prefix('login')->name('login.')->group(function () {
+        Route::get('/', [AuthController::class, 'index'])->name('index');
+        Route::post('/', [AuthController::class, 'login'])->name('store');
+    });
 });

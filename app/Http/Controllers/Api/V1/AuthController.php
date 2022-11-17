ca881bf7ec7777bdb5e8bb319029e832ee5c3fdc
bohdan_hordiychuk <bohdan1217>
2022-11-17 10:54:46 +0200

    small fix

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Dto\Auth\AuthCreateApiTokenDto;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\AuthLoginRequest;
-use App\Http\Resources\UserLoginResource;
+use App\Http\Resources\UserResource;
 use App\Services\Auth\AuthServiceInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
@@ -15,15 +15,15 @@ class AuthController extends Controller
     /**
      * @param AuthLoginRequest $request
      * @param AuthServiceInterface $authService
-     * @return UserLoginResource|JsonResponse
+     * @return UserResource|JsonResponse
      */
     public function login(AuthLoginRequest $request, AuthServiceInterface $authService)
     {
         if (Auth::attempt($request->only('email', 'password', 'push_token'))) {
-           $user = Auth::user();
-           $token = $authService->createApiToken($user, AuthCreateApiTokenDto::createFromRequest($request));
+            $user = Auth::user();
+            $token = $authService->createApiToken($user, AuthCreateApiTokenDto::createFromRequest($request));
 
-            return (new UserLoginResource($user))->additional([
+            return (new UserResource($user))->additional([
                 'message' => trans('auth.success'),
                 'access_token' => $token->token,
                 'push_token' => $request->get('push_token'),

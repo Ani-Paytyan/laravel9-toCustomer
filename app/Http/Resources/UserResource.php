c11a6115cf57b08589f537217036762451f20ba6
bohdan_hordiychuk <bohdan1217>
2022-11-18 16:18:24 +0200

    add documentation

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -3,10 +3,43 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Schema(
+ *     title="User Resource",
+ *     type="object",
+ *     schema="UserResource",
+ *
+ *     @OA\Property(
+ *         type="string",
+ *         property="name",
+ *         title="Name",
+ *         example="Name",
+ *     ),
+ *
+ *     @OA\Property(
+ *         type="string",
+ *         property="lastName",
+ *         title="Last Name",
+ *         example="LastName",
+ *     ),
+ *
+ *     @OA\Property(
+ *         type="string",
+ *         property="email",
+ *         title="Email Address",
+ *         example="test@example.com",
+ *     ),
+ * )
+ */
 class UserResource extends JsonResource
 {
-    public function toArray($request)
+    /**
+     * @param $request
+     * @return array
+     */
+    public function toArray($request): array
     {
         return [
             'name' => $this->resource->getFirstName(),

1aaca5c5780244e502961590fe27005f767126ac
araqe <arman.arakelyan@develit.se>
2022-11-23 12:24:13 +0400

    fix

--- config/iwms.php
+++ config/iwms.php
@@ -3,6 +3,6 @@
 return [
     'api_base_url' => env('IWMS_API_BASE_URL', ''),
     'root_token' => env('IWMS_ROOT_TOKEN', ''),
-    'current_user_token' =>env('IWMS_API_USER_TOKEN', ''),
+    'current_user_token' => null,
     'system' => env('IWMS_API_SYSTEM', 'customer'),
 ];
